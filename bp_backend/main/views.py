aed75bff2ea61b8930c065ec8cde06ba78d80297
Tammer <tammer@tammer.com>
2022-06-18 11:48:32 -0400

    first API

--- bp_backend/main/views.py
+++ bp_backend/main/views.py
@@ -1,3 +1,14 @@
+from urllib import response
+from attr import attributes
 from django.shortcuts import render
+from yaml import serialize
+from .serializers import AttributeSerializer
+from .models import Attribute
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class Attributes(APIView):
+    def get(self, request):
+        attributes = Attribute.objects.all()
+        serializer = AttributeSerializer(attributes,many=True)
+        return Response(serializer.data)
