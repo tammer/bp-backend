d665f5951279bd6985dba704edc5020aee43312f
Tammer <tammer@tammer.com>
2022-06-22 13:20:42 -0400

    first pass at base models

--- bp_backend/main/models.py
+++ bp_backend/main/models.py
@@ -1,5 +1,16 @@
-from unicodedata import category
 from django.db import models
+from accounts.models import BPUser
+
+class Level(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20,unique=True)
+    def __str__(self):
+        return self.name
+
+class Skill(models.Model):
+    name = models.CharField(max_length=20,unique=True)
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=40, unique=True)
@@ -16,6 +27,24 @@ class Attribute(models.Model):
 
     def __str__(self):
         return self.name
+
+class AnchorTable(models.Model):
+    passer =  models.ForeignKey(BPUser, related_name='passer_anchor_table', on_delete=models.CASCADE)
+    receiver =  models.ForeignKey(BPUser, related_name='receiver_anchor_table',  on_delete=models.CASCADE)
+    skill = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    def __str__(self):
+        return self.created_at
+
+class AnchorInvite(models.Model):
+    passer =  models.ForeignKey(BPUser, on_delete=models.CASCADE)
+    receiver_email = models.EmailField()
+    skill = models.ForeignKey(Skill,on_delete=models.CASCADE)
+    level = models.ForeignKey(Level,on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    def __str__(self):
+        return self.created_at
 
 class Profile(models.Model):
     owner = models.OneToOneField('accounts.BPUser', on_delete=models.CASCADE)
